ed176919a8320f1cc69c44fdb6e3309a8e0f6b1e
27akim <criminal.ok1@gmail.com>
2021-03-02 22:30:34 +0300

    feat: update js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 module.exports = function towelSort (matrix) {
-  if (matrix==undefined || matrix==[])
-  return [];
-  else{
-  let arr=[];
-  for (let i=0; i<matrix.length; i++)
-    if((i+1)%2 == 1)
-    {
-      arr=arr.concat(matrix[i]);
-      console.log(arr);
-    }
-    else
-    {
-      arr=arr.concat(matrix[i].reverse());
-      console.log(arr);
-    }
-    return arr;
+  for(let i = 1; i< matrix.length; i+=2)
+  {
+    matrix[i] = matrix[i].reverse();
   }
+  return matrix.flat();
 }
