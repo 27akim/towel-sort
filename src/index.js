4768a814630d0d27c42ebf173dd47bb4abdfb0d8
27akim <criminal.ok1@gmail.com>
2021-03-02 22:34:00 +0300

    feat: update js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 module.exports = function towelSort (matrix) {
-  for(let i = 1; i< matrix.length; i+=2)
+  if(matrix != undefined)
+  {
+    for(let i = 1; i< matrix.length; i+=2)
   {
     matrix[i] = matrix[i].reverse();
   }
   return matrix.flat();
+  }
+  else
+  {
+    return [];
+  }
+  
 }
