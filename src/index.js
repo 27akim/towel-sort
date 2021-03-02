ca8b4245bd330b70c62a742770b24c6ebf096688
27akim <criminal.ok1@gmail.com>
2021-03-02 22:15:42 +0300

    feat: update js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 module.exports = function towelSort (matrix) {
-  let m = [];
-  for(let i of matrix.flat())
-  {
-    m.push(i);
-  }
-  m.sort(function(a,b){ 
-    return a - b;
-  })
+  let m = [].concat(...matrix);
   return m;
 }
